4245dc526465974ca136d7d568bc203e6d1fc874
Patrick Rodacker <patrick@rodacker.de>
2019-11-04 22:37:23 +0100

    pulls up proxy function to card contract

--- src/Contracts/Card.php
+++ src/Contracts/Card.php
@@ -3,10 +3,17 @@
 namespace Memory\Contracts;
 
 use Memory\Card\CardId;
+use Memory\Card\Content\ContentId;
 
 interface Card
 {
     public function id(): CardId;
 
     public function content(): string;
+
+    public function title(): string;
+
+    public function contentId(): ContentId;
+
+    public function contentType(): string;
 }
